36da74481946e861deb8caa1bf2e914670de785f
Andreas Becker <andreas@becker.name>
2017-12-27 11:22:30 +0100

    Fixed encoding to UTF-8

--- java8/src/test/java/java8/chapter2_lambdas/Aufgabe01.java
+++ java8/src/test/java/java8/chapter2_lambdas/Aufgabe01.java
@@ -5,37 +5,37 @@ import org.junit.Test;
 import java8.chapter2_lambdas.aufgabe01.LongBinaryOperator;
 
 /**
- * Lösung zu Aufgabe 1
+ * LÃ¶sung zu Aufgabe 1
  */
 public class Aufgabe01 {
 
-	@Test
-	public void aufgabe01() {
+    @Test
+    public void aufgabe01() {
 
-		// ACHTUNG: Typ Long ist nicht erlaubt!
-		/*
-		 * final LongBinaryOperator v1 = (long x, Long y) -> { return x + y; };
-		 */
+	// ACHTUNG: Typ Long ist nicht erlaubt!
+	/*
+	 * final LongBinaryOperator v1 = (long x, Long y) -> { return x + y; };
+	 */
 
-		final LongBinaryOperator v2 = (long x, long y) -> {
-			return x + y;
-		};
+	final LongBinaryOperator v2 = (long x, long y) -> {
+	    return x + y;
+	};
 
-		final LongBinaryOperator v3 = (long x, long y) -> x + y;
+	final LongBinaryOperator v3 = (long x, long y) -> x + y;
 
-		// ACHTUNG: Wenn, dann muss die Typdeklaration für BEIDE Parameter
-		// weggelassen werden!
-		/*
-		 * final LongBinaryOperator v4 = (long x, y) -> x + y;
-		 */
+	// ACHTUNG: Wenn, dann muss die Typdeklaration fÃ¼r BEIDE Parameter
+	// weggelassen werden!
+	/*
+	 * final LongBinaryOperator v4 = (long x, y) -> x + y;
+	 */
 
-		final LongBinaryOperator v5 = (x, y) -> x + y;
+	final LongBinaryOperator v5 = (x, y) -> x + y;
 
-		// ACHTUNG: Die Parameter-Klammern dürfen bei mehr als einem Parameter
-		// NICHT weggelassen werden!
-		/*
-		 * final LongBinaryOperator v6 = x, y -> x + y;
-		 */
-	}
+	// ACHTUNG: Die Parameter-Klammern dÃ¼rfen bei mehr als einem Parameter
+	// NICHT weggelassen werden!
+	/*
+	 * final LongBinaryOperator v6 = x, y -> x + y;
+	 */
+    }
 
 }
